fdd98b1657d842c270001b217d40c3ba2b8467bb
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-03-27 14:08:07 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let url = 'https://newsapi.org/v2/top-headlines?sources=hacker-news&apiKey=8e1b3f61c5fa41aeac3ea4638086a011';
+
+fetch(url)
+  .then(r => {
+  return r.json();
+  })
+  .then(data => {
+  let results = data.articles;
+  let newsList = document.createElement("ol");
+  let body = document.querySelector("body");
+  body.appendChild(newsList);
+  results.map(news => {
+    let newsItem = document.createElement("li");
+    newsItem.innerHTML = '<a href="' + news.href + '">' + news.title + "</a>";
+    newsList.appendChild(newsItem);
+  });
+})
+  .catch(e => {
+  console.log(`An error occured: ${e}`);
+})
